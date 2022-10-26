10d82fa9c08739b7603eff1193110c422262c54c
unknown <juan.molina-l@labinfo.is.escuelaing.edu.co>
2022-10-26 17:24:56 -0500

    noEntregable

---
+++ src/edu/is/escuelaing/arep/api/model/Palindromo.java
@@ -0,0 +1,28 @@
+package edu.is.escuelaing.arep.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Palindromo {
+    private String palabra;
+
+    public Palindromo(){
+        this.palabra = palabra;
+    }
+    public String palabraAlcontrario (){
+        String palabraA = palabra;
+
+
+
+
+
+
+
+
+        }
+        ;
+
+    }
+    }
+}
